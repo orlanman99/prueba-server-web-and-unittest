1f667b77fb04e09d61af0d3ec843ec3f906ef34e
Yeison Manrique <yeisonmanrique@usantotomas.edu.co>
2020-08-11 22:19:11 -0500

    version-1

--- Database/redis.ts
+++ Database/redis.ts
@@ -8,5 +8,5 @@ interface storage{
 export const redisStorage: storage = {
     get: (list_name: string) => [],
     add: (list_name: string, item: string) => false,
-    remove: (list_name: string, item: string) => false,
+    remove: (list_name: string, item: string) => false
 };
